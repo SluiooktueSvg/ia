e851153a807514c4fae47cd825afd41f6522e710
Luis Canchila <canchilamario100@gmail.com>
2025-06-17 20:53:29 +0000

    ey chat, agrega un tipo de letra de google fonts para greetin (saludo)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ export default {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
+        greeting: ['"Fredoka One"', 'cursive'],
         code: ['monospace'],
       },
       colors: {
