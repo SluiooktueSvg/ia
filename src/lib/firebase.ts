6a70da2338b304dd9a210d8facd44dbe4537d617
Luis Canchila <canchilamario100@gmail.com>
2025-08-13 01:01:10 +0000

    mira, aun sigue saliendo el error:
    7:58:07 PM: Export encountered an

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -13,6 +13,6 @@ const firebaseConfig = {
 };
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase for client-side rendering
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
